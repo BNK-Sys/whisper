e99221379661ab85cfeaab2cd8f9b6b56f35c2a8
jybin96 <jybin96@changwon.ac.kr>
2024-07-05 14:48:31 +0900

    feat: main UI 구축단계

---
+++ frontend/src/component/main-page/credit-score/CreditScore.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./CreditScore.css"
+
+const CreditScore = () => {
+  return (
+    <div className='credit_score_main'>
+        <div className='credit_score_title'></div>
+    </div>
+  )
+}
+
+export default CreditScore
