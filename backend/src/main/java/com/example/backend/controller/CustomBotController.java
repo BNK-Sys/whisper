009c9461dc12349d3c0d09bb0ef6c6e9d75763b6
toki0411 <toki402@naver.com>
2024-07-05 15:05:40 +0900

    feat: chatgpt 연결

---
+++ backend/src/main/java/com/example/backend/controller/CustomBotController.java
@@ -0,0 +1,35 @@
+package com.example.backend.controller;
+
+
+
+import com.example.backend.dto.ChatGPTRequest;
+import com.example.backend.dto.ChatGPTResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/bot")
+@CrossOrigin("*")
+public class CustomBotController {
+    @Value("${openai.model}")
+    private String model;
+
+    @Value("${openai.api.url}")
+    private String apiURL;
+
+    @Autowired
+    private RestTemplate template;
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam(name = "prompt") String prompt) {
+        ChatGPTRequest request = new ChatGPTRequest(model, prompt);
+        ChatGPTResponse chatGPTResponse = template.postForObject(apiURL, request, ChatGPTResponse.class);
+        return chatGPTResponse.getChoices().get(0).getMessage().getContent();
+    }
+}
