14899a4b87c415a73ad95af64b798a67045081d2
toki0411 <toki402@naver.com>
2024-07-12 13:21:56 +0900

    feat: 성공음

--- frontend/src/pages/Compelete.jsx
+++ frontend/src/pages/Compelete.jsx
@@ -1,20 +1,31 @@
 import React, { useEffect } from 'react'
 import CompeleteContent from '../component/compelete-page/CompeleteContent';
 import { getSpeech } from '../component/commons/tts/TTS';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom'
+import Success from "../assets/success_sound.mp3";
+
 const Compelete = () => {
 
   {/* TTS 기능 */}
   const voiceValue = '송금이 완료되었습니다. 잠시 후 메인 페이지로 이동합니다.';
   const navigate = useNavigate();
 
-  
   let tts = false;
+  const audioRef = useRef(new Audio(Success));
+
   useEffect(() => {
     if(!tts) {
       tts = true;
       getSpeech(voiceValue);
       window.speechSynthesis.getVoices();
+
+      const audioElement = audioRef.current;
+      audioElement.play();
+      return () => {
+        audioElement.pause();
+        audioElement.currentTime = 0;
+      };
+
     }
   }, [])
 
